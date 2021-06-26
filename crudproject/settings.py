5edfb90418dd2cb0e62f8694c4e6a62732391801
전계선 <dbtjkss@gmail.com>
2021-06-26 22:40:39 +0900

    코멘트

--- crudproject/settings.py
+++ crudproject/settings.py
@@ -20,20 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = 'django-insecure-l44+38uinzc_^2fr*o%(cs=st&e2*m1iwu9+3lz@l)kg7qzcj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
